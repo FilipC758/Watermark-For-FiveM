161e0341e4fd3bb3baf3be0382fd27ae3a0b7ca2
Filip <143913150+FilipC758@users.noreply.github.com>
2023-12-14 02:47:53 +0000

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+luad54 'yes'
+
+author 'Filip'
+description 'Watermark For FiveM!'
+version '1.0.0'
+
+client_script {
+    'client/client.lua',
+}
+
+
+ui_page 'nui/nui.html'
+
+files {
+    'nui/nui.html',
+    'nui/style.css',
+    'nui/script.js',
+    'nui/images/*',
+}
